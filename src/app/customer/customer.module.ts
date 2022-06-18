535463a579a12a013f802bdbcf9213989036e8f7
Anurag94Singh <asanuragsingh44@gmail.com>
2022-06-18 19:44:38 +0530

    4 files modified

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -5,22 +5,17 @@ import { LandingComponent } from './components/landing/landing.component';
 import { LookUpFormComponent } from './components/look-up-form/look-up-form.component';
 import { NewCustomerComponent } from './components/new-customer/new-customer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
-    BrowserAnimationsModule,
     CustomerRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     MatSelectModule,
   ],
-  declarations: [
-    LandingComponent,
-    LookUpFormComponent,
-    NewCustomerComponent
-  ]
+  declarations: [LandingComponent, LookUpFormComponent, NewCustomerComponent],
 })
-export class CustomerModule { }
+export class CustomerModule {}
