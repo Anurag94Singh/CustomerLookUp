5147297523ed8d03201c560e6f130986efadfa15
Anurag94Singh <asanuragsingh44@gmail.com>
2022-06-18 19:34:53 +0530

    1 file modified

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,11 +4,14 @@ import { CustomerRoutingModule } from './customer-routing.module';
 import { LandingComponent } from './components/landing/landing.component';
 import { LookUpFormComponent } from './components/look-up-form/look-up-form.component';
 import { NewCustomerComponent } from './components/new-customer/new-customer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     LandingComponent,
