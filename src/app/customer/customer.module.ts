92742b4d538817f1f6bea370713924d8e0630f6e
Anurag94Singh <asanuragsingh44@gmail.com>
2022-06-18 19:32:57 +0530

    1 file modified

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { LandingComponent } from './components/landing/landing.component';
 import { LookUpFormComponent } from './components/look-up-form/look-up-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NewCustomerComponent } from './components/new-customer/new-customer.component';
 
 @NgModule({
